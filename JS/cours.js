9035fcccfdff1d886600bb7ea88391bfc55e351a
Papiscocisse <143758558+Aliou221@users.noreply.github.com>
2024-10-07 04:01:17 +0000

    Update cours.js

--- JS/cours.js
+++ JS/cours.js
@@ -6,6 +6,37 @@ let userName = document.querySelector('.username');
 let userEmail = document.querySelector('.email');
 let moduleName = document.querySelector('.modulename');
 
-userName.innerHTML = donnee[0].username;//Vas prendre le nom de l'utilisateur apres connexion 
-userEmail.innerHTML = donnee[0].usermail;//Vas prendre le email de l'utilisateur apres connexion
-moduleName.innerHTML = module.Module;
+let imgProfile = document.querySelector('.img-profile');
+let changeProfile = document.querySelector('#profile');
+
+
+if(donnee !== null && module !== null){
+
+    userName.innerHTML = donnee[0].username;//Vas prendre le nom de l'utilisateur apres connexion 
+    userEmail.innerHTML = donnee[0].usermail;//Vas prendre le email de l'utilisateur apres connexion
+    moduleName.innerHTML = module.Module;
+}
+
+changeProfile.addEventListener('change' , ()=>{
+    
+    let reader = new FileReader();
+    let file = changeProfile.files[0];
+
+    reader.addEventListener('loadend', ()=>{
+        
+        imgProfile.src = reader.result;
+
+        localStorage.setItem('img', reader.result);
+    })
+    
+    reader.readAsDataURL(file);
+
+})
+
+let locale = localStorage.getItem('img');
+
+if(locale !== null){
+    imgProfile.src = locale;
+}else{
+    imgProfile.src = "../img/user.png"
+}
